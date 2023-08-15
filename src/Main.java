c19e04319743eeb84b7daa320bf5b884dd7f7c81
Ekaterina Kononova <ekaterina.panova@mail.ru>
2023-08-15 14:47:16 +0500

    max commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 
 public class Main {
-
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
@@ -12,37 +12,27 @@ public class Main {
 
         long startTs = System.currentTimeMillis(); // start time
 
-        List<Thread> threads = new ArrayList<>();
+        final ExecutorService threadPool = Executors.newFixedThreadPool(25); // в лекции было озвучено, что еще есть специальный метод, который спрашивает у ОС про кол-во ядер для исполнения
+
+        List<Future<Integer>> threadsCallable = new ArrayList<>();
 
         for (String text : texts) {
-            Thread thread = new Thread(() -> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
-                }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            });
-            threads.add(thread);
-            thread.start();
+            Callable<Integer> myCallable = new MyCallable(text);
+            final Future<Integer> task = threadPool.submit(myCallable); // отправляю в пул потоков
+            threadsCallable.add(task); // добавляю в список
         }
 
-        for (Thread thread : threads) {
-            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
+        int maxSize = 0;
+        for (Future<Integer> future : threadsCallable) {
+            int currentMaxSize = future.get(); // получаем результат от потока
+            if (currentMaxSize > maxSize) {
+                maxSize = currentMaxSize;
+            }
         }
+
+        threadPool.shutdown(); // завершаем работу потоков
+
+        System.out.println("maxSize " + maxSize);
 
         long endTs = System.currentTimeMillis(); // end time
 
